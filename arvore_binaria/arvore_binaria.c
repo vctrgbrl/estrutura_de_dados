d4aa6beaae096d81347474612e0c6a29be1cb70a
vsant <vgsantanasilva@id.uff.br>
2023-08-30 07:33:51 -0300

    first

---
+++ arvore_binaria/arvore_binaria.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct arvore_binaria {
+    int info;
+    struct arvore_binaria *esq, *dir;
+} arvore_binaria;
+
+void arvore_binaria_clone() {
+    
+}
+
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
+}
